30b35a6d2cfc2580a8789d57da4f53d269aafa3f
Fernando Nascimento <fernando_dev@outlook.com.br>
2020-08-25 19:13:23 -0300

    criando DAO do objeto agente

--- app/src/main/java/br/fernando/agendaprospera/model/AgenteDAO.kt
+++ app/src/main/java/br/fernando/agendaprospera/model/AgenteDAO.kt
@@ -1,2 +1,42 @@
 package br.fernando.agendaprospera.model
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+class AgenteDAO(context: Context) {
+
+    val repositorio = Repositorio(context)
+    var db: SQLiteDatabase = TODO()
+
+    /**
+     * funcao para abrir o banco de dados
+     */
+    fun abrirBanco(): SQLiteDatabase {
+        if (db == null) {
+            db = repositorio.writableDatabase
+        }
+        return db
+    }
+
+    /**
+     * funcao para ler o banco de dados
+     */
+
+    fun lerBanco(): SQLiteDatabase {
+        if (db == null) {
+            db = repositorio.readableDatabase
+        }
+        return db
+    }
+
+    /**
+     * funcao para fechar o banco de dados
+     */
+
+    fun fecharBanco() {
+        repositorio.close()
+    }
+
+
+
+}
